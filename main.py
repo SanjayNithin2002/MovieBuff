02b15053db7b7b918ce48976b0abcc88e020de04
Sanjay Nithin <sanjaynithin.s2020@vitstudent.ac.in>
2021-02-18 21:30:57 +0000

    design

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
 import discord
 import imdb
 import os
-from urllib.request import urlretrieve
 from keep_alive import keep_alive
-
 client = discord.Client()
 ia = imdb.IMDb()
 def movie(a):
   b = ia.search_movie(a)[0]
   code = b.movieID
   series = ia.get_movie(code)
-  if 'cover url' in series:
-    url = series['cover url']
-    urlretrieve(url, 'pic.jpg')
-
-  rating = series.data['rating']
-  year = series.data['year']
+  if 'cover url' in series: url = series['cover url']
+  cast = ",".join([str(i) for i in series.data['cast'][:3]])
+  rating = cast + "\n" + str(series.data['rating']) +"\n" + str(series.data['year'])
   plot = series.data['plot'][0].split("::")[0]
-  cast = [i['name'] for i in series.data['cast'][:3]]
-  
-  try: 
-    director = series.data['director'][0] 
-  except KeyError: 
-    director = ""
-  return (b,rating,year,plot,cast,director)
-
-  
- 
+  return (rating,plot,url,b)
 def person(a):
   b = ia.search_person(a)[0]
   str1 = ia.get_person_filmography(b.personID)
@@ -46,18 +32,13 @@ def person(a):
         str2+="{0}.{1}\n".format(index+1,movie_name)
   return str2
 def top(a):
-  if a=='movies':
-    t = ia.get_top250_movies()
-  if a=='shows':
-    t = ia.get_top250_tv()
-  if a=='popular':
-    t = ia.get_popular100_movies()
+  if a=='movies': t = ia.get_top250_movies()
+  if a=='shows': t = ia.get_top250_tv()
+  if a=='popular': t = ia.get_popular100_movies()
   str1=''
-  for i in t[:10]:
-    str1+='{0}\n'.format(i)
+  for i in t[:10]: str1+='{0}\n'.format(i)
   return str1
 dict1=dict()
-
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
@@ -66,21 +47,17 @@ async def on_ready():
 async def on_message(message):
     t = message.guild.id
     if t not in dict1: dict1[t]=[]
-
+    if message.content.startswith("$"):
+      await message.add_reaction("👍")
     if message.author == client.user:
         return
     if message.content.startswith("$film") or message.content.startswith("$tv"):
       a = message.content[3:]
       result = movie(a)
-      embedVar = discord.Embed(title="", description="",color=0xF6BE00)
-      embedVar.add_field(name="Starring", value=",".join(result[4]),inline=False)
-      embedVar.add_field(name="Synopsis", value=result[3], inline=False)
-      embedVar.add_field(name="Year", value=result[2], inline=False)
-      embedVar.add_field(name="Rating", value=result[1], inline=False)
-      if result[-1]!="":
-        embedVar.add_field(name="Director", value=result[-1], inline=False)
+      embedVar = discord.Embed(title=str(result[3]), description=str(result[0]),color=0xF6BE00) 
+      embedVar.set_footer(text=result[1])
+      embedVar.set_thumbnail(url=result[2])
       await message.channel.send(embed=embedVar)
-      await message.channel.send(file=discord.File('pic.jpg'))
     if message.content.startswith("$list"):
       a = message.content[3:]
       result = person(a)
